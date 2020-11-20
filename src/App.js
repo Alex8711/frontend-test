93834c67045ee69bacb33720f5b6b8d7141b1380
Alex Liu <liuzhendell@gmail.com>
2020-11-19 23:00:39 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header  from './components/shared/Header/Header'
+import HomePage from './screens/HomePage/HomePage'
+import CourseSummaryStudent from './components/CourseSummaryStudent/CourseSummaryStudent'
+import CourseSummaryInstructor from './components/CourseSummaryInstructor/CourseSummaryInstructor'
+import HomeworkSummaryStudent from './components/HomeworkSummaryStudent/HomeworkSummaryStudent';
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={HomePage} /> 
+          <Route path="/student" exact component={CourseSummaryStudent} /> 
+          <Route path="/instructor" exact component={CourseSummaryInstructor} /> 
+          <Route path="/studenthomework" exact component={HomeworkSummaryStudent} /> 
+        </Switch>
+      </Router>
+    </>
   );
 }
 
